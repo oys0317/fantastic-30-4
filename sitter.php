46c838a8ee6085a7fe01a14b3ab75cdc855a97be
Jenny <jenny.jieun.song@gmail.com>
2015-06-12 14:31:51 -0700

    add bootstrap

---
+++ sitter.php
@@ -0,0 +1,27 @@
+<head>
+	<link rel="stylesheet" href="bootstrap.min.css">
+	<title>PetSitter</title>
+</head>
+<body>
+	<nav class="navbar navbar-inverse navbar-fixed-top">
+		<div class="container">
+	    	<a class="navbar-brand" href="index.php">PETCARE</a>
+			<form class="navbar-form navbar-right">
+        		<div class="form-group">
+         			<input type="text" class="form-control" placeholder="id">
+         			<input type="text" class="form-control" placeholder="password">
+        		</div>
+        		<button type="submit" class="btn btn-default">Submit</button>
+      		</form>
+      	</div>
+	</nav>
+	<div style="padding: 80px 0; background-color:5cb85c; !important" class="jumbotron">
+  		<div class="container">
+  			<h1 style="color:white">Pet Sitters</h1>
+  			<p style="color:white">Cras venenatis ullamcorper diam vel accumsan. Morbi elit ipsum, semper vitae erat at, semper finibus risus. In vitae rhoncus ipsum. Aliquam sit amet finibus massa. Morbi non risus eu nibh ullamcorper hendrerit vel vitae mauris. Suspendisse ut felis placerat ante vehicula euismod. Nunc ornare ipsum urna, eget finibus lacus efficitur id. Vivamus dapibus tempor augue at hendrerit. Integer tincidunt, turpis sit amet interdum pellentesque, eros est mollis libero, tempus ullamcorper dui sem a arcu. Maecenas fermentum pellentesque egestas. Aliquam euismod, lectus non elementum posuere, mi turpis interdum velit, quis bibendum ipsum enim vel lacus.</p>
+  		</div>
+	</div>
+	<div class="container">
+		list goes here
+	</div>
+</body>
