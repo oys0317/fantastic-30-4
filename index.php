955bb79485de592dcda8285e698e6c7c2a52d758
Jenny <jenny.jieun.song@gmail.com>
2015-06-12 03:55:44 -0700

    sitter and owner page

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 
 <head>
 	<link rel="stylesheet" href="bootstrap.min.css">
-	<title>PetSitter</title>
+	<title>PetCare</title>
 </head>
 <body>
 	<nav class="navbar navbar-inverse navbar-fixed-top">
@@ -21,7 +21,7 @@
 	</nav>
 	<div class="jumbotron" style="padding: 80px 0">
   		<div class="container">
-  			<h1>Pet Sitters</h1>
+  			<h1>PETCARE</h1>
   			<p>Cras venenatis ullamcorper diam vel accumsan. Morbi elit ipsum, semper vitae erat at, semper finibus risus. In vitae rhoncus ipsum. Aliquam sit amet finibus massa. Morbi non risus eu nibh ullamcorper hendrerit vel vitae mauris. Suspendisse ut felis placerat ante vehicula euismod. Nunc ornare ipsum urna, eget finibus lacus efficitur id. Vivamus dapibus tempor augue at hendrerit. Integer tincidunt, turpis sit amet interdum pellentesque, eros est mollis libero, tempus ullamcorper dui sem a arcu. Maecenas fermentum pellentesque egestas. Aliquam euismod, lectus non elementum posuere, mi turpis interdum velit, quis bibendum ipsum enim vel lacus.</p>
   			<div class="btn-group" role="group">
  				<a href="sitter.php" class="btn btn-success btn-lg">Pet Sitters</a>
