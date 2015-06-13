685fb9fa20a94b69034d72b97d1718b1d612fd23
Jenny <jenny.jieun.song@gmail.com>
2015-06-12 17:31:45 -0700

    aaaa

--- login.php
+++ login.php
@@ -3,3 +3,10 @@ password: <?php echo $_POST["password"]; ?>
 
 TODO: check if they match...
 
+
+<?php
+	$db = new PDO("mysql:host=localhost;dbname=fantastic304;port=3306","root");
+	//$db = new PDO("mysql:host=localhost;dbname=fantastic304;port=3306","root","root");
+
+	
+?>
