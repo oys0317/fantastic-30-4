4243210a32fcb97489b8ced7b18e0c6a19f7c845
Alyssa Lerner <alyssa.0189@gmail.com>
2015-06-14 17:22:05 -0700

    Changed id to password in header

--- include/header.php
+++ include/header.php
@@ -4,7 +4,7 @@
 			<?php if(!isset($_COOKIE['userID'])): ?>
 				<form class="navbar-form navbar-right" action= "<?php $_SERVER['DOCUMENT_ROOT'] ?>/login.php" method="post">
 	        		<div class="form-group">
-	         			<input type="text" class="form-control" name="id" placeholder="id">
+	         			<input type="text" class="form-control" name="id" placeholder="username">
 	         			<input type="password" class="form-control" name="password" placeholder="password">
 	        		</div>
 	        		<button type="submit" class="btn btn-warning">Submit</button>
