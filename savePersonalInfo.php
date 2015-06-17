3cc009f717c424d7ddaef57e383c2bddcaa206cb
Alyssa Lerner <alyssa.0189@gmail.com>
2015-06-17 16:32:15 -0700

    Fixed bug where unedited user fields disappear while editing info

--- savePersonalInfo.php
+++ savePersonalInfo.php
@@ -3,14 +3,33 @@
 $db = new PDO("mysql:host=localhost;dbname=fantastic304;port=3306","root");
 $userID = $_COOKIE['userID'];
 
-// Update the user's info
-$stmt = $db->prepare("	UPDATE User
-						SET Name=:Name, Address=:Address, PhoneNum=:PhoneNum
-						WHERE UserID = '$userID'");
-$stmt->bindParam(':Name', $_POST['editName']);
-$stmt->bindParam(':Address', $_POST['editAddress']);
-$stmt->bindParam(':PhoneNum', $_POST['editPhoneNum']);
-$stmt->execute();
+$name = $_POST['editName'];
+$address = $_POST['editAddress'];
+$phoneNumber = $_POST['editPhoneNum'];
+
+// Update the user's name if necessary
+if($name) {
+	$stmt = $db->prepare("	UPDATE User
+							SET Name='$name';
+							WHERE UserID = '$userID'");
+	$stmt->execute();
+}
+
+if($address) {
+	// Update the user's address if necessary
+	$stmt = $db->prepare("	UPDATE User
+							SET Address='$address';
+							WHERE UserID = '$userID'");
+	$stmt->execute();
+}
+
+if($phoneNumber) {
+	// Update the user's phone number if necessary
+	$stmt = $db->prepare("	UPDATE User
+							SET PhoneNum='$phoneNumber';
+							WHERE UserID = '$userID'");
+	$stmt->execute();
+}
 
 // TODO: Tell user error occurred if a field is blank
 
