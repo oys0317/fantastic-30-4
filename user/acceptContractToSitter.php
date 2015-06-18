96063465fe47c91feacbd2f4c9fef9e7dc85f3ab
g0c0b <younsukoh777@gmail.com>
2015-06-17 19:22:29 -0700

    done with accepting contract to Sitter

--- user/acceptContractToSitter.php
+++ user/acceptContractToSitter.php
@@ -2,21 +2,25 @@
 
 		$db = new PDO("mysql:host=localhost;dbname=fantastic304;port=3306","root");
 		$userID = $_COOKIE['userID'];
-
+		$OwnerID = $_GET['OwnerID'];
+		$AvailabilityID = $_GET['AvailabilityID'];
 		$sql = "UPDATE contracttositter 
 				SET status = 1
-				WHERE OwnerID = $userID 
+				WHERE OwnerID = '$OwnerID' 
 				AND SitterID = '$userID' 
-				AND AvailabilityID = :AvailabilityID";
+				AND AvailabilityID = '$AvailabilityID'";
 
 				// UPDATE contracttoowner 
 				// SET status = 1
 				// WHERE OwnerID = '$userID' 
 				// AND RequestID = :RequestID;
-		$stmt = $db->prepare($sql);
-		$stmt->bindParam(':AvailabilityID', $_POST['AvailabilityID']);
-		//$stmt->bindParam(':RequestID', $_POST['RequestID']);		
-		$stmt->execute();
+		$db->query($sql);
+
+		$sql = "DELETE FROM contracttositter 
+				WHERE OwnerID <> '$OwnerID'  
+				AND SitterID = '$userID'
+				AND AvailabilityID = '$AvailabilityID'";
+		$db->query($sql);
 
 		header('Location: ./inbox.php');
 ?>
