f9cfbdf0114870047291a731daf6234061ad0a25
Magnus <magnushvidsten@gmail.com>
2015-06-17 17:24:20 -0700

    further improvements on inbox feature

--- user/inbox.php
+++ user/inbox.php
@@ -3,9 +3,9 @@
 		try{
 			$userID = $_COOKIE['userID'];
 			$db = new PDO("mysql:host=localhost;dbname=fantastic304;port=3306","root");
-			$sql = "SELECT OwnerID, AvailabilityID, StartDate, EndDate, Compensation	
-					FROM contracttositter c
-					WHERE '$userID' = c.SitterID and c.Status = 0";
+			$sql = "SELECT c.OwnerID, f.species, f.size, c.AvailabilityID, c.StartDate, c.EndDate, c.Compensation	
+					FROM contracttositter c, cantakecareof f
+					WHERE '$userID' = c.SitterID and c.Status = 0 and f.availabilityID = c.availabilityID";
 			$row = $db->query($sql);
 			
 			// If there are no sitters, display a different message.
@@ -16,6 +16,14 @@
 			echo '<table class="table table-striped">';
 			echo '<th>';
 			echo "Name Of Owner";
+			echo '</th>';
+
+			echo '<th>';
+			echo "Pet Type";
+			echo '</th>';
+
+			echo '<th>';
+			echo "Pet Size";
 			echo '</th>';
 
 			echo '<th>';
@@ -40,6 +48,14 @@
 
 				echo '<td>';
 				echo $row['OwnerID'];
+				echo '</td>';
+
+				echo '<td>';
+				echo $row['species'];
+				echo '</td>';
+
+				echo '<td>';
+				echo $row['size'];
 				echo '</td>';
 
 				echo '<td>';
@@ -71,9 +87,9 @@
 		try{
 			$userID = $_COOKIE['userID'];
 			$db = new PDO("mysql:host=localhost;dbname=fantastic304;port=3306","root");
-			$sql = "SELECT SitterID, RequestID, StartDate, EndDate, Compensation	
-					FROM contracttoowner c
-					WHERE '$userID' = c.OwnerID and c.Status = 0";
+			$sql = "SELECT c.SitterID, f.PetName, c.RequestID, c.StartDate, c.EndDate, c.Compensation	
+					FROM contracttoowner c, ownspet f
+					WHERE '$userID' = c.OwnerID and c.Status = 0 and f.OwnerID = c.OwnerID";
 			$row = $db->query($sql);
 			
 			// If there are no sitters, display a different message.
@@ -84,6 +100,10 @@
 			echo '<table class="table table-striped">';
 			echo '<th>';
 			echo "Name Of Sitter";
+			echo '</th>';
+
+			echo '<th>';
+			echo "Pet Name";
 			echo '</th>';
 
 			echo '<th>';
@@ -108,6 +128,10 @@
 
 				echo '<td>';
 				echo $row['SitterID'];
+				echo '</td>';
+
+				echo '<td>';
+				echo $row['PetName'];
 				echo '</td>';
 
 				echo '<td>';
@@ -145,13 +169,18 @@
   		<div class="container">
   			<h1 style="color:white">Inbox</h1>
   			<p style="color:white">
-  					Here you can view offered contracts.
+  					Here you can view, accept or decline offered contracts.
   			</p>
   		</div>
 	</div>
 	<div class="container">
-		<font size="4"face="verdana" color="blue">Contracts offered to you. Here you can choose to accept or decline.</font>
+		<br><br>
+		<font size="5"face="verdana" color="black"><u>Contracts offered by people wanting to take care of your pet</u></font>
+		<br>
 		<?php ContractToOwnerTable();?>
+		<br><br>
+		<font size="5"face="verdana" color="black"><u>Contracts offered by people wanting you to take care of their pet</u></font>
+		<br>
 		<?php ContractToSitterTable();?>
 	</div>
 
